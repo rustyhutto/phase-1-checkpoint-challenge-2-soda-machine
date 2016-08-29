9f6fbedef4d77a38f429092fc8dd16712952dcb6
Rusty Hutto <email": null>
2016-08-29 15:00:09 -0700

    start of soda machine program

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,25 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    if @sodas.include?(soda_brand)
+      return @sodas.brand[soda_brand]
+    else
+      return nil
+    end
   end
 
   def sell(soda_brand)
+    if @sodas.include?(soda_brand)
+      @cash += soda_brand.price
+    else
+      return nil
+    end
   end
 
 end
+
+
